4bf3123b21feba58398377b34c02332c0be1e579
Ding <ding@DingdeMBP.hsd1.va.comcast.net>
2020-02-23 15:53:32 -0500

    models test added

--- isa/main/tests.py
+++ isa/main/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Users, Product, Review
 
-# Create your tests here.
+class UsersTest(TestCase):
+    #setUp method is called before each test in this class
+    def setUp(self):
+        pass #nothing to set up
+
+    def TestReadingUser(self):
+        response = self.client.get(reverse('all_users', kwargs={'user_id':1}))
+        self.assertContains(response, 'all_users')
+
+    def TestCreateUser(self):
+        
+
+
+    def tearDown(self):
+        pass #nothing to tear down
