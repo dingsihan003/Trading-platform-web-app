cb012578bba1146d596a02d7479a2ffa26c01502
Jiahe <jt7gu@virginia.edu>
2020-04-19 08:41:29 +0800

    try

--- selenium/selenium_test.py
+++ selenium/selenium_test.py
@@ -22,6 +22,32 @@ class WebFunctions(unittest.TestCase):
         print(driver)
         print(driver.title)
         self.assertIn("Market", driver.title)
+    
+    def test_signup_login(self):
+        driver = self.driver
+        driver.get("http://web1:8000/signup/")
+        username = driver.find_element_by_name("username")
+        password = driver.find_element_by_name("password")
+        email = driver.find_element_by_id("email")
+        location = driver.find_element_by_id("location")
+        submit = driver.find_element_by_id("submit")
+        email.send_keys("selenium@test.com")
+        username.send_keys("selenium")
+        password.send_keys('test')
+        submit.click()
+
+        driver.get("http://web1:8000/login")
+        driver.find_element_by_name("username").send_keys('selenium')
+        driver.find_element_by_name("password").send_keys('test')
+        driver.find_element_by_name('submit').click()
+        self.assertIn("Market", driver.title)
+
+        # driver.get('http://web1:8000/create_listing/')
+        # button = driver.find_element_by_id('navbarDropdown')
+        # button.click()
+        # signout = driver.find_element_by_id('signout')
+        # signout.click()
+        # self.assertIn('Profile', driver.find_element_by_id('navbarDropdown').text)
 
     def tearDown(self):
         self.driver.close()
