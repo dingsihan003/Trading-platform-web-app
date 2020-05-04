5fa2d70e9bd220ed8aa4cf9e0704a05a36a5fb99
Jiahe <jt7gu@virginia.edu>
2020-05-04 20:54:47 +0800

    compose

--- batch/update_counts.py
+++ batch/update_counts.py
@@ -15,7 +15,6 @@ def update_counts():
     user_id, item_id  = line.split()
     c[item_id].add(user_id)
   for item_id, users in c.items():
-    print(item_id, len(users))
     es.update(index='listing_index', doc_type='listing', id=item_id , body={ 'script' : 'ctx._source.visits = 0'})
     try:
       for i in range(len(users)):
@@ -24,16 +23,6 @@ def update_counts():
       print(str(e))
       time.sleep(1)
   f.close()
-
-# while(True):
-#     try:
-#         consumer = KafkaConsumer('listing', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
-#         print("log consumer in update_counts",consumer)
-#         break
-#     except Exception as e:
-#         print(str(e))
-#         print("log consumer failed in update_counts")
-#         time.sleep(3)
 while True:
   update_counts()
   time.sleep(60)
