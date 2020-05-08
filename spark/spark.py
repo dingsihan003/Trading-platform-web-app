53872587f65992d144cd373440e39b3fece4bcef
Jiahe <jt7gu@virginia.edu>
2020-05-08 16:37:12 +0800

    spark changed

--- spark/spark.py
+++ spark/spark.py
@@ -6,20 +6,18 @@ from itertools import combinations
 import urllib.request
 import urllib.parse
 
+f = open("/tmp/data/spark_ouput.log", "w")
+
 spkcontext = SparkContext("spark://spark-master:7077", "PopularItems")
 data = spkcontext.textFile("/tmp/data/access.log", 2) 
 pairs = data.map(lambda line: line.split())
 list_pairs = pairs.groupByKey()
-list_coview_pairs = list_pairs.flatMap(lambda line: [[line[0], coview] for coview in combinations(list(line[1]), 2)])
-reveser_list_coview_pairs = list_coview_pairs.map(lambda pair: (pair[1], pair[0]))
-coview_users = reveser_list_coview_pairs.groupByKey()
-count_coview = coview_users.map(lambda coview_list: (coview_list[0], len(coview_list[1])))
-count_coview_filtered = count_coview.filter(lambda pair: pair[1] >= 3)
+view_pairs = list_pairs.flatMap(lambda line: [[line[0], coview] for coview in combinations(list(line[1]), 2)])
+reveser_view_pairs = view_pairs.map(lambda pair: (pair[1], pair[0]))
+view_users = reveser_view_pairs.groupByKey()
+count = view_users.map(lambda view_list: (view_list[0], len(view_list[1])))
 
-
-f = open("/tmp/data/spark_ouput.log", "w")
-
-output = count_coview_filtered.collect()
+output = count.filter(lambda pair: pair[1] > 2).collect()
 table = collections.defaultdict(set)
 
 for pair, count in output:
